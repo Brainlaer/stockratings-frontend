ec0879cbb757b30534e8fada53d70e42559c56d0
brainlaer <branstivenson@gmail.com>
2025-03-10 10:01:32 -0500

    structure

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { PrimeIcons } from '@primevue/core/api';
+
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import StockRatingList from '../views/stockRating/StockRatingList.vue'
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/stock_rating/list',
+      name: 'stockRatingList',
+      component: StockRatingList
+    }
+  ],
+})
+
+export default router
