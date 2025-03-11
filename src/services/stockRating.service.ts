ef54443ee74b3ce032c74a4ac3029546f0ad0cc4
brainlaer <branstivenson@gmail.com>
2025-03-11 15:43:32 -0500

    add services for http requests

--- src/services/stockRating.service.ts
+++ src/services/stockRating.service.ts
@@ -1,9 +1,15 @@
 import axios from "axios"
+import type { ParamValue } from "vue-router"
 
 const baseUrl='http://localhost:8081/api'
 
 export const stockRatingService={
-    getAll(){
-        return axios.get(`${baseUrl}/v1/stock`)
+    getAll(params?:URLSearchParams){
+        console.log(params)
+        return axios.get(`${baseUrl}/v1/stock`, {params:params})
+    },
+    getHighestTagetsNow(){
+        return axios.get(`${baseUrl}/v1/stock?sortBy=target_to, time&order=desc, asc&offset=0&limit=7&rating_to=Hold`)
     }
+
 }
