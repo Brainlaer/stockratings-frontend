d436bf2984b5d7252d64751b140d30195ad4d0bd
brainlaer <branstivenson@gmail.com>
2025-03-11 15:42:29 -0500

    add  primevue and libraries

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
 import 'primeicons/primeicons.css';
+import ToastService from 'primevue/toastservice';
 
 import App from './App.vue'
 import router from './router'
@@ -17,6 +18,7 @@ app.use(PrimeVue, {
     },
 });
 app.use(PrimeVue)
+app.use(ToastService); 
 app.use(createPinia())
 app.use(router)
 app.mount('#app')
