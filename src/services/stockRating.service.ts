a5a9d790c51713d47928d324f8f3637c09f4c5a6
brainlaer <branstivenson@gmail.com>
2025-03-12 13:49:28 -0500

    add new http resquests

--- src/services/stockRating.service.ts
+++ src/services/stockRating.service.ts
@@ -5,11 +5,22 @@ const baseUrl='http://localhost:8081/api'
 
 export const stockRatingService={
     getAll(params?:URLSearchParams){
-        console.log(params)
         return axios.get(`${baseUrl}/v1/stock`, {params:params})
     },
     getHighestTagetsNow(){
         return axios.get(`${baseUrl}/v1/stock?sortBy=target_to, time&order=desc, asc&offset=0&limit=7&rating_to=Hold`)
+    },
+    getOne(id:string){
+        return axios.get(`${baseUrl}/v1/stock/${id}`)
+    },
+    update(body:any, id:string){
+        return axios.put(`${baseUrl}/v1/stock/${id}`,body)
+    },
+    create(body:any){
+        return axios.post(`${baseUrl}/v1/stock`,body)
+    },
+    delete(id:string){
+        return axios.delete(`${baseUrl}/v1/stock/${id}`)
     }
 
 }
