540b2d254165fd5603d0cae4be07ecf60042f268
brainlaer <branstivenson@gmail.com>
2025-03-11 23:02:52 -0500

    add new routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import StockRatingList from '../views/stockRating/StockRatingList.vue'
 import StockRatingDetail from '@/views/stockRating/StockRatingDetail.vue'
+import StockRatingEdit from '@/views/stockRating/StockRatingEdit.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -14,6 +15,14 @@ const router = createRouter({
       path: '/stock_rating',
       name: 'stockRatingList',
       component: StockRatingList
+    },{
+      path: '/stock_rating/edit/:id',
+      name: 'stockRatingEdit',
+      component:StockRatingEdit
+    },{
+      path: '/stock_rating/create',
+      name: 'stockRatingCreate',
+      component:StockRatingEdit
     },{
       path: '/stock_rating/:id',
       name: 'stockRatingDetail',
