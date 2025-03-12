efc08bafa72b6657a07bfd0ccc73f972ec3694ac
brainlaer <branstivenson@gmail.com>
2025-03-11 23:02:36 -0500

    add toast element

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
 import { PrimeIcons } from '@primevue/core/api';
+import { Toast } from 'primevue';
 import Navbar from './components/Navbar.vue';
 
 </script>
 
 <template>
+  <Toast />
     <div class=" flex-col w-screen-99">
       <div>
         <Navbar />
       </div>
-      <div>
+      <div class="p-1">
         <RouterView />
       </div>
     </div>
